93f354e92bb93095eab453b2079852ce861843c0
wbas-5969 <baslock@live.com>
2025-10-09 22:22:59 -0400

    finished lesson ch2l3

---
+++ functions/get_file_content.py
@@ -0,0 +1,32 @@
+import os
+
+def get_file_content(working_directory, file_path):
+    
+    file_relpath= working_directory + "/" + file_path
+    file_abspath = os.path.abspath(file_relpath)
+    print(f"file_abspath: {file_abspath}")
+    dir_abspath = os.path.abspath(working_directory)
+    print(f"dir_abspath: {dir_abspath}")
+    # If the absolute path to the directory is outside the working_directory, return a string error message:
+    if not file_abspath.startswith(dir_abspath):
+        return(f'Error: Cannot read "{file_abspath}" as it is outside the permitted working directory')
+
+    # If the directory argument is not a file, again, return an error string:
+    try:
+        if not os.path.isfile(file_abspath):
+            return(f'Error: File not found or is not a regular file: "{file_abspath}"')
+    except:
+        print(f"Error: system error returned by os.path.isfile({file_abspath})")
+
+    MAX_CHARS = 10000
+
+    with open(file_abspath, "r") as f:
+        try:
+            file_content_string = f.read(MAX_CHARS)
+        except:
+            print(f"Error: can't open file: {file_abspath}")
+
+    if len(file_content_string) >= MAX_CHARS:
+        return file_content_string + f"...File \"{file_abspath}\" truncated at 10000 characters"
+    else:
+        return file_content_string
--- tests.py
+++ tests.py
@@ -1,13 +1,33 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-test_result_1 = get_files_info("calculator", ".")
-print(f"{test_result_1}")
+# test_result_1 = get_files_info("calculator", ".")
+# print(f"{test_result_1}")
 
-test_result_2 = get_files_info("calculator", "pkg")
-print(f"{test_result_2}")
+# test_result_2 = get_files_info("calculator", "pkg")
+# print(f"{test_result_2}")
 
-test_result_3 = get_files_info("calculator", "/bin")
-print(f"{test_result_3}")
+# test_result_3 = get_files_info("calculator", "/bin")
+# print(f"{test_result_3}")
 
-test_result_4 = get_files_info("calculator", "../")
-print(f"{test_result_4}")
+# test_result_4 = get_files_info("calculator", "../")
+# # print(f"{test_result_4}")
+
+# get_files_content = get_file_content("./", "get_file_content.py")
+# # print(f"file content: {get_files_content}")
+
+# get_files_content = get_file_content("calculator", "lorem.txt")
+# print(f"file content: {get_files_content}")
+
+get_files_content = get_file_content("calculator", "main.py")
+print(f"file content: {get_files_content}")
+
+get_files_content = get_file_content("calculator", "pkg/calculator.py")
+print(f"file content: {get_files_content}")
+
+get_files_content = get_file_content("calculator", "/bin/cat")
+print(f"file content: {get_files_content}")
+
+get_files_content = get_file_content("calculator", "pkg/does_not_exist.py")
+print(f"file content: {get_files_content}")
+
